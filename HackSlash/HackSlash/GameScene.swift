423379d58c057d2651aa9e0bf9d30881fc13ffb2
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2023-04-24 17:29:04 -0300

    setting up xcode proj

---
+++ HackSlash/HackSlash/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  HackSlash
+//
+//  Created by Gabriel Medeiros Martins on 24/04/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
