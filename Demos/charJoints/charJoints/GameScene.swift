5d2c2a875827359bf4129980bf51c7c94fbede3e
Pedro Mezacasa Muller <pedro.mezzacasa@gmail.com>
2023-04-29 17:44:02 -0300

    boneco braco funcionando mais ou menos

--- Demos/charJoints/charJoints/GameScene.swift
+++ Demos/charJoints/charJoints/GameScene.swift
@@ -12,15 +12,11 @@ class GameScene: SKScene {
     var char = SKNode()
     var body = SKSpriteNode()
     var arm = SKSpriteNode()
+    var ground = SKSpriteNode()
+    
+    var pin = SKPhysicsJointPin()
     
     @objc private func onPan(_ recognizer: UIPanGestureRecognizer) {
-        if recognizer.state == .ended {
-            for i in char.children {
-                print(i)
-                print(i.children)
-            }
-        }
-        
         guard recognizer.state == .changed else {return}
         
         let location = scene?.convertPoint(fromView: recognizer.location(in: view))
@@ -28,48 +24,35 @@ class GameScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
-//        let origin = SKShapeNode(circleOfRadius: 5)
-//        origin.fillColor = .white
-//        scene?.addChild(origin)
         
-        // ---------------------------------------------------------------
-        body = SKSpriteNode(color: .white, size: CGSize(width: 100, height: 200))
-        body.name = "body"
+        
+        body = SKSpriteNode(color: .red, size: CGSize(width: 80, height: 160))
+        body.physicsBody = SKPhysicsBody()
+        body.physicsBody?.allowsRotation = false
+        body.physicsBody?.affectedByGravity = false
+        body.physicsBody?.isDynamic = true
         char.addChild(body)
+        addChild(char)
         
-        // ---------------------------------------------------------------
-//        let armAnchor = CGPoint(x: 75, y: 50)
-//        let anchorDebug = SKShapeNode(circleOfRadius: 5)
-//        anchorDebug.position = armAnchor
-//        scene?.addChild(anchorDebug)
+        arm = SKSpriteNode(color: .white, size: CGSize(width: 20, height: 80))
+        arm.position = CGPoint(x: 60, y: 0)
+        arm.physicsBody = SKPhysicsBody()
+        arm.physicsBody?.allowsRotation = true
+        arm.physicsBody?.affectedByGravity = false
+        arm.physicsBody?.isDynamic = true
+        char.addChild(arm)
         
-        // ---------------------------------------------------------------
-//        arm = SKShapeNode(rectOf: CGSize(width: 35, height: 100))
-//        arm.position = CGPoint(x: 75, y: 0)
-//        scene?.addChild(arm)
-        let armHolder = SKNode()
-        armHolder.name = "armHolder"
-        body.addChild(armHolder)
-        armHolder.position = CGPoint()
-
-        arm = SKSpriteNode(color: .red, size: CGSize(width: 35, height: 100))
-        armHolder.addChild(arm)
-        arm.anchorPoint = CGPoint(x: 0.5, y: 1)
-        arm.position = CGPoint(x: 75, y: 50)
-        arm.name = "arm"
-
         
-        // ---------------------------------------------------------------
-        let gr = UIPanGestureRecognizer(target: self, action: #selector(onPan))
-        view.addGestureRecognizer(gr)
         
-        // ---------------------------------------------------------------
+        arm.anchorPoint = CGPoint(x: 0.5, y: 0)
+        pin = SKPhysicsJointPin.joint(withBodyA: arm.physicsBody!, bodyB: body.physicsBody!, anchor: body.position)
         
-        scene?.addChild(char)
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(onPan))
+        self.view?.addGestureRecognizer(panRecognizer)
     }
     
     override func update(_ currentTime: TimeInterval) {
-//        arm.zRotation += CGFloat.pi / 60
-//        body.children[0].zRotation += CGFloat.pi / 60
+        arm.zRotation += CGFloat.pi / 60
+        char.position.x += frame.width * 0.001
     }
 }
