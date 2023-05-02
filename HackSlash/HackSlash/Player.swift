2a7612d73ac210b5ae7fc2b2467c9e80ca03247a
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2023-05-02 17:03:08 -0300

    created preliminar player class

---
+++ HackSlash/HackSlash/Player.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  HackSlash
+//
+//  Created by Gabriel Medeiros Martins on 02/05/23.
+//
+
+import Foundation
+import SpriteKit
+
+class Player {
+    var sprite: SKSpriteNode
+    
+    var physicsBody: SKPhysicsBody {
+        sprite.physicsBody!
+    }
+    
+    var position: CGPoint {
+        sprite.position
+    }
+    
+    init(sprite: String) {
+        self.sprite = SKSpriteNode(imageNamed: sprite)
+        self.sprite.size = CGSize(width: 30, height: 60)
+        self.sprite.physicsBody = SKPhysicsBody()
+    }
+}
+
+
+
