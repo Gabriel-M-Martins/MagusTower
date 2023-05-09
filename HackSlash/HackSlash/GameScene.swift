39ccf3ae0eeda0085f0a8757c5063a4277215c45
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2023-05-09 11:37:30 -0300

    preliminar map interpreter

--- HackSlash/HackSlash/GameScene.swift
+++ HackSlash/HackSlash/GameScene.swift
@@ -6,8 +6,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return Constants(frame: frame)
     }
 
-    // lista de plataformas existentes na cena. todas sao desenhadas no começo do jogo
-    var platforms: [SKSpriteNode] = []
     var player: Player = Player(sprite: "")
     var spider: EnemySpider = EnemySpider(sprite: "", attributes: AttributesInfo(health: 10, defense: 1, weakness: [], velocity: VelocityInfo(xSpeed: 0, ySpeed: 0, maxXSpeed: 0, maxYSpeed: 0)))
     var toDie: Int = 3
@@ -17,16 +15,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var movementInput = SKShapeNode()
     private var combosInput = SKShapeNode()
     
+    private var frameMultiplier: CGFloat = 4
+    
     @objc private func handleTap(_ recognizer: UITapGestureRecognizer) {
         let tappedNode = atPoint(convertPoint(fromView: recognizer.location(in: view)))
         
         guard let nodeName = tappedNode.name else { return }
         
         if nodeName == "movementInput" {
-            print(player.currentState)
             player.move(direction: [.up])
             player.transition(to: .jump)
-            print(player.currentState)
             return
         }
         
@@ -35,19 +33,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             return
         }
     }
-    
 
     /// quando a view chamar a cena, esta funçao é a primeira a ser executada.
     ///  é a preparaçao da cena.
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
+        
+        self.setScale(4)
+        
         background.zPosition = -10
-        background.size = CGSize(width: frame.width * 2, height: frame.height * 2)
+        background.size = CGSize(width: frame.width, height: frame.height)
+//        background.size = CGSize(width: frame.width * frameMultiplier, height: frame.height * frameMultiplier)
         addChild(background)
+        
         // ------------------------------------------------------------------------
-        setupGround()
+//        setupGround()
+        setupGround2()
         // ------------------------------------------------------------------------
-        setupPlayer()
+//        setupPlayer()
         // ------------------------------------------------------------------------
         setupCamera()
         // ------------------------------------------------------------------------
@@ -67,18 +70,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 spider.transition(to: .charging)
             }
             player.transition(to: .idle)
-            print("foi")
         }
         
         if (contact.bodyA.node?.name == "platform" && contact.bodyB.node?.name == "Player") || (contact.bodyA.node?.name == "Player" && contact.bodyB.node?.name == "platform") {
-            print(player.currentState)
             player.transition(to: .idle)
-            print(player.currentState)
         }
     }
     
+    
+    func setupGround2() {
+        guard let mapInterpreter = MapInterpreter(map: frame, platformHeight: frame.height/10) else { return }
+        
+        for i in mapInterpreter.rects {
+            self.createPlatform(size: i.size, position: i.position, sprite: "UnderGroundReal")
+        }
+        
+    }
+    
     override func update(_ currentTime: TimeInterval) {
-//        player.move(direction: [.right, .up])
         camera?.position = player.position
     }
     
@@ -105,7 +114,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func setupCamera() {
         let camera = SKCameraNode()
-        camera.setScale(0.7)
+        camera.setScale(2)
+//        camera.setScale(0.7)
         self.camera = camera
         addChild(camera)
     }
@@ -114,7 +124,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let platform = SKSpriteNode(imageNamed: sprite)
         platform.size = size
         // settando o anchor point para ser no meio horizontal e no baixo na vertical
-        platform.anchorPoint = CGPoint(x: 0.5, y: 0)
+        platform.anchorPoint = CGPoint(x: 0, y: 0)
+//        platform.anchorPoint = CGPoint(x: 0.5, y: 0)
         // posicao do platform é zero no x e o mais baixo no y
         platform.position = position
         // criando o physicsbody e settando que nao é dinamico p nenhuma força poder ser aplicada contra ele
@@ -135,11 +146,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //cria plataforma direita
         createPlatform(size: CGSize(width: frame.width/3, height: constants.platformsHeight), position: CGPoint(x: frame.maxX - frame.width/6, y: frame.midY), sprite: "UnderGroundReal")
         // ------------------------------------------------------------------------
-        
-        // adicionando todas as plataformas como childs da cena
-        for i in platforms {
-            addChild(i)
-        }
     }
     
     func setupPlayer(){
@@ -153,5 +159,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         spider = EnemySpider(sprite: spriteName, attributes: AttributesInfo(health: 10, defense: 20, weakness: [], velocity: VelocityInfo(xSpeed: -5, ySpeed: 10, maxXSpeed: 5, maxYSpeed: 10)))
         spider.sprite.position = position
         addChild(spider.sprite)
+    }
+}
+
+
+extension CGRect {
+    mutating func scale(by value: Float) {
+        self.size = CGSize(width: size.width * CGFloat(value), height: size.height * CGFloat(value))
     }
 }
