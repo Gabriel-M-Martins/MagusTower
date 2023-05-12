906a8a302c72f3f80e60b26b4d4c64b0602327ad
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2023-05-12 15:04:55 -0300

    implemented interface

---
+++ HackSlash/HackSlash/Views/MenuView.swift
@@ -0,0 +1,86 @@
+//
+//  MenuView.swift
+//  menu
+//
+//  Created by Mateus Moura Godinho on 10/05/23.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct MenuView: View {
+    @State var showSettings: Bool = false
+    @State var showCredits: Bool = false
+
+    var body: some View {
+        NavigationStack {
+            GeometryReader { geo in
+                ZStack{
+                    Image("MenuBackground").resizable().scaledToFill()
+                        .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).midY)
+                    
+                    ZStack{
+                        Image("Correntes").edgesIgnoringSafeArea(.all)
+                            .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).midY*0.83)
+                        
+                        Group{
+                            NavigationLink {
+                                MainView()
+                            } label: {
+                                Image("Enter")
+                            }
+                            .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).minY + geo.frame(in: .global).height*0.31)
+//
+//                            Button(action:{
+//                            }, label:{
+//                                Image("Enter")
+//                            })
+//                            
+                            
+                            Button(action:{
+                                showSettings = !showSettings
+                            }, label:{
+                                Image("Settings")
+                            })
+                            .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).minY + geo.frame(in: .global).height*0.72)
+                            
+                            Button(action:{
+                                
+                            }, label:{
+                                Image("How")
+                            })
+                            .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).minY + geo.frame(in: .global).height*0.55)
+                        }
+                        
+                        Button(action:{
+                            showCredits = !showCredits
+                        }, label:{
+                            Image("Credits")
+                        })
+                        .position(x: geo.frame(in: .global).minX+geo.frame(in: .global).width*0.2, y: geo.frame(in: .global).maxY-20)
+                        
+                        Image("FrameBotoes").edgesIgnoringSafeArea(.all)
+                            .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).minY + geo.frame(in: .global).height*0.12)
+                        
+                        if(showSettings){
+                            SettingsView(showSettings: $showSettings)
+                        }
+                        
+                        if(showCredits){
+                            CreditsView(showCredits: $showCredits)
+                        }
+                    }
+                }
+            }
+            .edgesIgnoringSafeArea(.all)
+//            .navigationBarBackButtonHidden()
+        }
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+            .previewInterfaceOrientation(.landscapeRight)
+    }
+}
