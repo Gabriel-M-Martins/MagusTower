3b3846784910ee3fd0eecadb8fb3ed5ecc717951
Pedro Mezacasa Muller <pedro.mezzacasa@gmail.com>
2023-05-12 15:03:32 -0300

    consertado bugs

--- HackSlash/HackSlash/GameViewController.swift
+++ HackSlash/HackSlash/GameViewController.swift
@@ -27,7 +27,6 @@ class GameViewController: UIViewController {
             
             view.showsFPS = false
             view.showsNodeCount = false
-            view.showsPhysics = true
         }
     }
 
