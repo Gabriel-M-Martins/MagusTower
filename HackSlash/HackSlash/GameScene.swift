79350c2273574524bc8598943804972827397ad4
Lucas Cunha <lucascunha@10-41-49-95.0200.epoa.br.iosda.org>
2023-05-15 12:07:08 -0700

    feat: combo timer

--- HackSlash/HackSlash/GameScene.swift
+++ HackSlash/HackSlash/GameScene.swift
@@ -99,26 +99,39 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     private func handleCombo(start: CGPoint, pos: CGPoint) {
+        if directionsCombos.count == 0{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    if self.directionsCombos.count <= 1 {
+                        print("Miss timing!")
+                        self.directionsCombos.removeAll()
+                    }
+            }
+        }
         let vector = pos - start
         let directions = Directions.calculateDirections(vector)
+        
         if directionsCombos.count == 2 {
             let normalizedVector = vector.normalized()
             let magic = Magics.magic(primary: directionsCombos[0], secondary: directionsCombos[1])
             let angle = atan2(normalizedVector.y, normalizedVector.x)
+            
             // MARK: call combo with vector and magic
             switch magic{
             case .A(.fire):
                 let fireball = Fireball(angle: angle, player: player)
                 magics.append(fireball)
                 addChild(fireball.node)
+                directionsCombos.removeAll()
             case .A(.ice):
                 let iceball = Iceball(angle: angle, player: player)
                 magics.append(iceball)
                 addChild(iceball.node)
+                directionsCombos.removeAll()
             default:
                 break
             }
             directionsCombos = []
+            
         } else {
             directionsCombos.append(directions[0])
         }
