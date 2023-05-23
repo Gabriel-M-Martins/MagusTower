17a37ab966dfa44036dc53a9c5e77789089fad83
felipeelsnersilva15@gmail.com <Felipe Elsner Silva>
2023-05-23 14:44:30 -0300

    spider adapted

--- HackSlash/HackSlash/Data/LevelInfo.swift
+++ HackSlash/HackSlash/Data/LevelInfo.swift
@@ -22,25 +22,25 @@ enum Levels: CaseIterable {
         case .Tutorial:
             return LevelInfo(background: "MainScene", enemiesQtd: 1, mapFile: "map2", spawnRate: 1)
         case .Level1:
-            return LevelInfo(background: "MainScene", enemiesQtd: 3, mapFile: "map1", spawnRate: 9)
+            return LevelInfo(background: "MainScene", enemiesQtd: 3, mapFile: "map1", spawnRate: 7)
         case .Level2:
-            return LevelInfo(background: "MainScene", enemiesQtd: 5, mapFile: "map4", spawnRate: 9)
+            return LevelInfo(background: "MainScene", enemiesQtd: 5, mapFile: "map4", spawnRate: 7)
         case .Level3:
-            return LevelInfo(background: "MainScene", enemiesQtd: 7, mapFile: "map1", spawnRate: 9)
+            return LevelInfo(background: "MainScene", enemiesQtd: 7, mapFile: "map1", spawnRate: 7)
         case .Level4:
-            return LevelInfo(background: "MainScene", enemiesQtd: 10, mapFile: "map3", spawnRate: 9)
+            return LevelInfo(background: "MainScene", enemiesQtd: 10, mapFile: "map3", spawnRate: 7)
         case .Level5:
-            return LevelInfo(background: "MainScene", enemiesQtd: 5, mapFile: "map5", spawnRate: 7)
+            return LevelInfo(background: "MainScene", enemiesQtd: 5, mapFile: "map5", spawnRate: 5)
         case .Level6:
-            return LevelInfo(background: "MainScene", enemiesQtd: 7, mapFile: "map4", spawnRate: 7)
+            return LevelInfo(background: "MainScene", enemiesQtd: 7, mapFile: "map4", spawnRate: 5)
         case .Level7:
-            return LevelInfo(background: "MainScene", enemiesQtd: 10, mapFile: "map6", spawnRate: 7)
-        case .Level8:
-            return LevelInfo(background: "MainScene", enemiesQtd: 5, mapFile: "map7", spawnRate: 5)
-        case .Level9:
-            return LevelInfo(background: "MainScene", enemiesQtd: 7, mapFile: "map3", spawnRate: 5)
-        case .Level10:
             return LevelInfo(background: "MainScene", enemiesQtd: 10, mapFile: "map6", spawnRate: 5)
+        case .Level8:
+            return LevelInfo(background: "MainScene", enemiesQtd: 5, mapFile: "map7", spawnRate: 4)
+        case .Level9:
+            return LevelInfo(background: "MainScene", enemiesQtd: 7, mapFile: "map3", spawnRate: 4)
+        case .Level10:
+            return LevelInfo(background: "MainScene", enemiesQtd: 10, mapFile: "map6", spawnRate: 4)
         }
     }
     
